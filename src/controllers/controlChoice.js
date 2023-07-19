8ec01a02b0bcfa4f50328fc41d27aa9b155e5960
Philippe <philippe.idalgoprestes@gmail.com>
2023-07-19 20:08:58 -0300

    Desenvolvimento completo

--- src/controllers/controlChoice.js
+++ src/controllers/controlChoice.js
@@ -1,9 +1,85 @@
+import { MongoClient, ObjectId } from "mongodb"
+import dotenv from "dotenv"
+import joi from "joi"
+import dayjs from "dayjs"
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+var db;
+mongoClient.connect().then(db = mongoClient.db());
+
 export function postChoice(req, res){
-    // Valida o _id e o title (ñao pode haver dois); é a função que cria as opções a serem votadas na enquete
-    // salva no db e retorna
+    const body = req.body;
+    const pollId = body.pollId;
+    const title = body.title;
+    const joiTitle = joi.string();
+    const { error, value } = joiTitle.validate(title);
+    if (error == null){
+        db.collection("Polls").findOne({ _id: new ObjectId(pollId) })
+            .then(resposta => {
+                if (resposta != null){
+                    if (dayjs() <= dayjs(resposta.expireAt)){
+                        db.collection("Choices").find({ pollId: new ObjectId(pollId) }).toArray()
+                            .then(choices => {
+                                function salvaDB(){
+                                    db.collection("Choices").insertOne(body)
+                                        .then(continua => {
+                                            db.collection("Choices").findOne(body)
+                                                .then(choice => res.send(choice).status(201).end())
+                                                .catch(err => console.log(err.message));
+                                            })
+                                        .catch(err => console.log(err.message));
+                                }
+                                if (choices.length != 0){
+                                    for (let a = 0; a < choices.length; a++){
+                                        if (choices[a].title == title){
+                                            res.status(409).end();
+                                        }
+                                        else if (a == choices.length - 1 && choices[a].title != title){
+                                            salvaDB()
+                                        }
+                                    }
+                                }
+                                else{
+                                    salvaDB();
+                                }
+                            })
+                            .catch(err => console.log(err.message));
+                    }
+                    else{
+                        res.status(403).end()
+                    }
+                }
+                else{
+                    res.status(404).end();
+                }
+            })
+            .catch(err => console.log(err.message));
+    }
+    else{
+        res.status(422).end();
+    }
+
 }
 
 export function postChoiceVote(req, res){
-    // Valida a enquete e o voto
-    // Salva na DB e retorna o status
+    db.collection("Choices").findOne({ _id: new ObjectId(req.params.id) })
+        .then(choice => {
+            if (choice != null){
+                db.collection("Polls").findOne({ _id: new ObjectId(choice.pollId) })
+                    .then(poll => {
+                        if (dayjs() <= dayjs(poll.expireAt)){
+                            const voteObject = {createdAt: dayjs().format("YYYY-MM-DD HH:mm"), choiceId: choice._id.toString()};
+                            db.collection("Votes").insertOne(voteObject)
+                                .then(final => res.status(201).end())
+                                .catch(err => console.log(err.message));
+                        }
+                        else{res.status(403).end()};
+                    })
+                    .catch(err => console.log(err.message));
+            }
+            else{res.status(404).end()};
+        })
+        .catch(err => console.log(err.message));
 }
--- src/controllers/controlPoll.js
+++ src/controllers/controlPoll.js
@@ -1,17 +1,88 @@
+import { MongoClient, ObjectId } from "mongodb"
+import dayjs from "dayjs"
+import dotenv from "dotenv"
+import joi from "joi"
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+var db;
+mongoClient.connect().then(db = mongoClient.db());
+
 export function postPoll(req, res){
-    // Valida o title e atribui o valor de expiredAt se necessário
-    // IF : se não tiver erro, salvar e retornar a enquete
+    const body = req.body;
+    const title = body.title;
+    let expireAt = body.expireAt;
+    const joiTitle = joi.string();
+    const { error, value } = joiTitle.validate(title);
+    if (error == null){
+        function salvaDB(){
+            const newObject = {title: title, expireAt: expireAt};
+            db.collection("Polls").insertOne(newObject)
+                .then(continua => {
+                    db.collection("Polls").findOne(newObject)
+                        .then(resposta => {
+                            res.send(resposta).status(201).end();
+                        })
+                        .catch(err => console.log(err.message));
+                })
+                .catch(err => console.log(err.message));
+        }
+        if (expireAt == null || expireAt == ""){
+            expireAt = dayjs().add(30, "day").format("YYYY-MM-DD HH:mm");
+            salvaDB();
+        }
+        else{
+            salvaDB();
+        }
+    }
+    else{
+        res.status(422).end();
+    }
 }
 
 export function getPoll(req, res){
-    // Encontra as enquetes no DB e retorna
+    db.collection("Polls").find().toArray()
+        .then(resposta => {
+            res.send(resposta).end();
+        })
+        .catch(err => console.log(err.message));
 }
 
 export function getPollChoice(req, res){
-    // Verifica a existência da enquete
-    // retorna erro ou a enquete
+    db.collection("Choices").find({ pollId: req.params.id }).toArray()
+        .then(list => {
+            if (list != null && list != []){
+                res.send(list).end();
+            }
+            else{
+                res.status(404).end();
+            }
+        })
+        .catch(err => console.log(err.message));
 }
 
 export function getPollResult(req, res){
-    // Pesquisa as opções no DB e pega a com maior voto, retornando-a ao usuário
+    db.collection("Choices").find({ pollId: req.params.id }).toArray()
+        .then(choices => {
+            let biggest = {votes: 0, title: ""};
+            for (let a = 0; a < choices.length; a++){
+                db.collection("Votes").find({ choiceId: choices[a]._id.toString() }).toArray()
+                    .then(votes => {
+                        if (votes.length > biggest.votes){
+                            biggest.votes = votes.length;
+                            biggest.title = choices[a].title;
+                        }
+                    })
+                    .catch(err => console.log(err.message));
+            }
+            db.collection("Polls").findOne({ _id: new ObjectId(req.params.id) })
+                .then(poll => {
+                    let resultObject = {title: biggest.title, votes: biggest.votes};
+                    const object = {_id: poll._id.toString(), title: poll.title, expireAt: poll.expireAt, result: [resultObject]};
+                    res.send(object).end();
+                })
+                .catch(err => console.log(err.message));
+        })
+        .catch(err => console.log(err.message));
 }
